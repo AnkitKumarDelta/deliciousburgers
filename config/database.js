8eb4bdbfbd7ae792fd5482341c121c99d206f91c
AnkitKumarDelta <ankitna355@gmail.com>
2024-03-18 19:37:52 +0530

    initial commit

---
+++ config/database.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Timeout after 5s
+      // Specify the host and port directly (remove/comment out the MONGO_URI)
+      // host: 'localhost',
+      // port: 27017,
+    });
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1); // Exit process with failure
+  }
+};
